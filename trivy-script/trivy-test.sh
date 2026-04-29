677414701fc1cb0a15b827c1fe81a7f13b2faf0a
ku1an <simondanielssoncsgo@gmail.com>
2026-04-29 21:42:35 +0200

    Fix Wazuh decoding bug, adding | as delimeter

--- trivy-script/trivy-test.sh
+++ trivy-script/trivy-test.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-images=$(docker images --format "{{.Repository}}:{{.Tag}}")
 
+images=$(docker images --format "{{.Repository}}:{{.Tag}}")
 TEMPLATE_DIR="/tmp"
 TEMPLATE_FILE="$TEMPLATE_DIR/trivy.tpl"
 
 cat <<EOL > "$TEMPLATE_FILE"
 {{- range \$r := . }}
   {{- range .Vulnerabilities }}
-"{{ .PkgName }}","{{ .VulnerabilityID }}","{{ .Severity }}","{{ .InstalledVersion }}","{{ .Title }}","{{ .PrimaryURL }}"
+{{ .PkgName }}|{{ .VulnerabilityID }}|{{ .Severity }}|{{ .InstalledVersion }}|{{ .Title }}|{{ .PrimaryURL }}|
   {{- end }}
 {{- end }}
 EOL
-
 
 if [ -z "$images" ]; then
   echo "No docker images found. Exists..."
@@ -21,7 +20,7 @@ fi
 for image in $images; do
     echo "-------------------------------------------------------------------------------------------------------------------------------------"
     echo "Image", "Package", "Vulnerability ID", "Severity", "Installed Version", "Description", "Solution Reference"
-    trivy i --quiet --format template --template "@/tmp/trivy.tpl" --scanners vuln $image | awk 'NF' | sed "s|^|Trivy: \"$image\",|"
+    trivy i --quiet --format template --template "@/tmp/trivy.tpl" --scanners vuln $image | awk 'NF' | sed "s|^|Trivy: \|$image\||"
     echo 
 done
 
